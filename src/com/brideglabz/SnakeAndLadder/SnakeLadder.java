992b74ce7b2d877d8534c8950fbf48576e5e6e0e
usha-12 <usha12ahirwar@gmail.com>
2023-01-04 21:04:36 +0530

    snake and ladder game played withh single player at start position 0

--- src/com/brideglabz/SnakeAndLadder/SnakeLadder.java
+++ src/com/brideglabz/SnakeAndLadder/SnakeLadder.java
@@ -1,2 +1,17 @@
-package com.brideglabz.SnakeAndLadder;public class SnakeLadder {
+package com.brideglabz.SnakeAndLadder;
+
+public class SnakeLadder {
+    public static final int POSITION = 0;
+    public static void main(String[] args) {
+        System.out.println("--------------welcome to Snake and Ladder------------");
+        System.out.println("Let's begin the game ....");
+        System.out.println("Initial position is Square Zero");
+        System.out.println("position : Square "+POSITION);
+
+
+    }
 }
+/*Snake and Ladder
+game played with
+single player at start
+position 0*/
