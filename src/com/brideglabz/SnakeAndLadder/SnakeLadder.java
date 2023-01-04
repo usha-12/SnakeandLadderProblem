e0c2a5828b22df9330395c2ee0eeb05b523cc5d7
usha-12 <usha12ahirwar@gmail.com>
2023-01-04 21:31:13 +0530

    Repeat till the player reaches the winning position 100

--- src/com/brideglabz/SnakeAndLadder/SnakeLadder.java
+++ src/com/brideglabz/SnakeAndLadder/SnakeLadder.java
@@ -1,36 +1,41 @@
 package com.brideglabz.SnakeAndLadder;
 
+import java.util.Random;
+
 public class SnakeLadder {
     public static void main(String[] args) {
-        int playOption =0;
+        int choice = 0;
         int POSITION = 0;
         int rollDice = 0;
-        playOption = (int)(Math.random() *10) % 3;
-        switch (playOption){
-            case 0:
-                System.out.println("You encountered a LADDER");
-                System.out.println("you can move" +rollDice+ "places ahead.");
-                POSITION = POSITION + rollDice;
-                break;
-            case 1:
-                System.out.println("you encountered a SNAKE");
-                System.out.println("you will move" +rollDice+ "places back.");
-                POSITION = POSITION - rollDice;
-                break;
-            case 2:
-                System.out.println("You got NO PLAY");
-                System.out.println("You will have to skip your turn.");
+        int dice = 0;
+        for (dice = 1; POSITION < 100; dice++) {
+            System.out.println();
+            rollDice = (int)((Math.random()*6)+1);
+            System.out.println("Rolled Dice Value :"+rollDice);
+            choice = (int) (Math.random() * 10) % 3;//check case
+            Random random = new Random();
+            switch (choice) {
+                case 0:
+                    System.out.println("You encountered a LADDER");
+                    System.out.println("you can move" +rollDice+ "places ahead.");
+                    POSITION = POSITION + rollDice;
+                    break;
+                case 1:
+                    System.out.println("you encountered a SNAKE");
+                    System.out.println("You will move " + rollDice +" places back.");
+                    POSITION = POSITION - rollDice;
+                    break;
+                default:
+                    System.out.println("You got NO PLAY");
+                    System.out.println("You will have to skip your turn.");
+            }
+            if (POSITION < 0)
+                POSITION = 0;
+            System.out.println("Current Position : Square " + POSITION);
         }
-        if (POSITION < 0)
-            POSITION = 0;
-        System.out.println("Current Position : Square " +POSITION);
     }
 }
-/*The Player then checks for
-an Option. They are No Play,
-Ladder or Snake. - Use ((RANDOM)) to check for Options - In Case of No Play the player stays in the same
-position
-- In Case of Ladder the player moves ahead by the
-number of position received in the die
-
-- In Case of Snake the player moves behind by the*/
+/*Repeat till the Player
+reaches the winning
+position 100. - Note In case the player position moves
+below 0, then the player restarts from 0*/
